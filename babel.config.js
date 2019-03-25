c5c69ab7af6afefcbb8d48bb9650e61f3cca0f4f
xiongotom <xiongotom@gmail.com>
2019-03-25 23:31:45 +0800

    开始pizza组件开发，遇到动态加载的问题

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,7 @@ module.exports = {
   presets: [
     '@vue/app'
   ]
+  // plugins: [
+  //   ['import', { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": "css" }]
+  // ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
